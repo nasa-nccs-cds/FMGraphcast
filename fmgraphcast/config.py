5d333f69bb712a3261a60d1d896d7d2051a6c958
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-03 15:04:03 -0600

    Integrate with fmbase

--- fmgraphcast/config.py
+++ fmgraphcast/config.py
@@ -1,5 +1,7 @@
-from graphcast.graphcast import ModelConfig, TaskConfig, GraphCast
-from pathlib import Path
+from graphcast.graphcast import ModelConfig, TaskConfig
+from fmbase.util.config import Configuration
+from omegaconf import DictConfig, OmegaConf
+
 def config_model( **kwargs ) -> ModelConfig:
 	opts = dict(
 		resolution=     kwargs.get('resolution',    cfg().model.resolution),
@@ -20,32 +22,9 @@ def config_task( **kwargs) -> TaskConfig:
 	    input_duration=     kwargs.get('input_duration',     f"{cfg().task.input_steps*dts}h" ) )
 	return TaskConfig(**opts)
 
-import hydra, os
-from omegaconf import DictConfig, OmegaConf
-from pathlib import Path
-
 def cfg() -> DictConfig:
     return Configuration.instance().cfg
 
 def configure(config_name: str, config_path=None):
     if config_path is None: config_path =  "../config"
     Configuration.init( config_name, config_path )
-
-class Configuration:
-    _instance = None
-    _instantiated = None
-
-    def __init__(self, config_name: str, config_path: str ):
-        hydra.initialize( version_base=None, config_path=config_path )
-        self.cfg: DictConfig = hydra.compose( config_name, return_hydra_config=True )
-
-    @classmethod
-    def init(cls, config_name: str, config_path: str ):
-        if cls._instance is None:
-            inst = cls(config_name,config_path)
-            cls._instance = inst
-            cls._instantiated = cls
-
-    @classmethod
-    def instance(cls) -> "Configuration":
-        return cls._instance
