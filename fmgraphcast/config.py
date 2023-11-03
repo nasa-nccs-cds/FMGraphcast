91b4e0a0324da72cc432a76842c6951cde3554f0
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-03 15:07:31 -0600

    Integrate with fmbase

--- fmgraphcast/config.py
+++ fmgraphcast/config.py
@@ -1,5 +1,5 @@
 from graphcast.graphcast import ModelConfig, TaskConfig
-from fmbase.util.config import Configuration
+import hydra, os
 from omegaconf import DictConfig, OmegaConf
 
 def config_model( **kwargs ) -> ModelConfig:
@@ -28,3 +28,22 @@ def cfg() -> DictConfig:
 def configure(config_name: str, config_path=None):
     if config_path is None: config_path =  "../config"
     Configuration.init( config_name, config_path )
+
+class Configuration:
+    _instance = None
+    _instantiated = None
+
+    def __init__(self, config_name: str, config_path: str ):
+        hydra.initialize( version_base=None, config_path=config_path )
+        self.cfg: DictConfig = hydra.compose( config_name, return_hydra_config=True )
+
+    @classmethod
+    def init(cls, config_name: str, config_path: str ):
+        if cls._instance is None:
+            inst = cls(config_name,config_path)
+            cls._instance = inst
+            cls._instantiated = cls
+
+    @classmethod
+    def instance(cls) -> "Configuration":
+        return cls._instance
