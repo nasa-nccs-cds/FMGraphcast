b41e923c6d9ec5df78445fe9dc0caf11a783a6de
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-16 14:11:10 -0700

    Add small era5 forecast demo.

--- fmgraphcast/config.py
+++ fmgraphcast/config.py
@@ -1,6 +1,8 @@
-from graphcast.graphcast import ModelConfig, TaskConfig
 from fmbase.util.config import cfg
 from fmbase.util.ops import fmbdir
+from graphcast import checkpoint
+from typing import Any, Mapping, Sequence, Tuple, Union, Dict
+from graphcast.graphcast import ModelConfig, TaskConfig, CheckPoint
 
 def config_model( **kwargs ) -> ModelConfig:
 	opts = dict(
@@ -27,4 +29,17 @@ def dataset_path() -> str:
 	res, levels, steps = cfg().model.res, cfg().model.levels, cfg().model.steps
 	year, month, day = cfg().model.year, cfg().model.month, cfg().model.day
 	return f"{root}/data/era5/res-{res}_levels-{levels}_steps-{steps:0>2}/{year}-{month:0>2}-{day:0>2}.nc"
+
+def config_files() -> Tuple[ModelConfig,TaskConfig]:
+    root = fmbdir('model')
+    params_file = cfg().task.params
+    pfilepath = f"{root}/params/{params_file}.npz"
+    with open(pfilepath, "rb") as f:
+        ckpt = checkpoint.load(f, CheckPoint)
+        model_config = ckpt.model_config
+        task_config = ckpt.task_config
+        print("Model description:\n", ckpt.description, "\n")
+        print(f" >> model_config: {model_config}")
+        print(f" >> task_config:  {task_config}")
+    return model_config, task_config
 
