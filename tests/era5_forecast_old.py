2685b47126628a74018567a66d9c5e47a86ae65b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-19 12:57:25 -0700

    Add small era5 forecast demo.

--- tests/era5_forecast_old.py
+++ tests/era5_forecast_old.py
@@ -124,10 +124,10 @@ def run_forward(model_config, task_config, inputs, targets_template, forcings):
 	predictor = construct_wrapped_graphcast(model_config, task_config)
 	print( f"\n Run forward-> inputs:")
 	for vn, dv in inputs.data_vars.items():
-		print(f" > {vn}{dv.dims}: {dv.shape}, std={dv.values.std()}")
+		print(f" > {vn}{dv.dims}: {dv.shape}, std={dv.std()}")
 	print( f"\n Run forward-> targets_template:")
 	for vn, dv in targets_template.data_vars.items():
-		print(f" > {vn}{dv.dims}: {dv.shape}, std={dv.values.std()}")
+		print(f" > {vn}{dv.dims}: {dv.shape}, std={dv.std()}")
 	return predictor(inputs, targets_template=targets_template, forcings=forcings)
 
 
