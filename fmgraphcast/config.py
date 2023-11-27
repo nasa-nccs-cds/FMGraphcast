382c5d97f9302e95b4a00b77ddd9dc04167b4dfb
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-27 15:53:29 -0700

    Add small era5 forecast demo.

--- fmgraphcast/config.py
+++ fmgraphcast/config.py
@@ -14,7 +14,8 @@ def config_model( **kwargs ) -> ModelConfig:
 		gnn_msg_steps=  kwargs.get( 'gnn_msg_steps', cfg().model.gnn_msg_steps   ),
 		hidden_layers=  kwargs.get( 'hidden_layers', cfg().model.hidden_layers   ),
 		radius_query_fraction_edge_length= kwargs.get( 'radius_query_fraction_edge_length', cfg().model.radius_query_fraction_edge_length ) )
-	print( f"\n --- config_model: {opts}")
+	ctypes = {k: type(v) for k, v in opts.items()}
+	print( f"\n --- config_model: {ctypes}")
 	return ModelConfig(**opts)
 
 def config_task( **kwargs) -> TaskConfig:
@@ -25,7 +26,8 @@ def config_task( **kwargs) -> TaskConfig:
 	    forcing_variables=  kwargs.get('forcing_variables',  cfg().task.forcing_variables),
 	    pressure_levels=    kwargs.get('levels',             cfg().task.levels),
 	    input_duration=     kwargs.get('input_duration',     f"{cfg().task.input_steps*dts}h" ) )
-	print(f"\n --- config_task: {opts}")
+	ctypes = { k:type(v) for k,v in opts.items() }
+	print(f"\n --- config_task: {ctypes}")
 	return TaskConfig(**opts)
 
 def dataset_path(**kwargs) -> str:
