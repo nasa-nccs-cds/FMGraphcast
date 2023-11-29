ab2f2572d0c8fc7c9dd620d0a2a3c32de533b467
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-29 11:55:29 -0700

    Add small era5 forecast demo.

--- tests/era5_data_test.py
+++ tests/era5_data_test.py
@@ -129,5 +129,5 @@ init_jitted = jax.jit(with_configs(run_forward.init))
 if params is None:
 	params, state = init_jitted( rng=jax.random.PRNGKey(0), inputs=train_inputs, targets_template=train_targets, forcings=train_forcings)
 
-pspec = { k: v['w'].shape for k,v in params.items() }
+pspec = { k: list(v.keys()) for k,v in params.items() }   # ['w'].shape
 print( f"Weights: {pspec}" )
