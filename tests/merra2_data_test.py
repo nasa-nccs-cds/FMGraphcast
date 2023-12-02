bcd868d2d95d283427ab90038300e63109a182d4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-02 14:02:29 -0700

    Add small era5 forecast demo.

--- tests/merra2_data_test.py
+++ tests/merra2_data_test.py
@@ -60,8 +60,8 @@ eval_lead_times =   slice("6h", f"{eval_steps*6}h") #[ f"{iS*dts}h" for iS in ra
 
 print( "  --------------------- MERRA2 ---------------------")
 example_batch: xa.Dataset = load_batch( year, month, day, ndays, cfg().task )
-time: np.ndarray = example_batch.coords['time'].values
-print( f" Batch time coord({time.dtype}): {time.tolist()}")
+time: np.ndarray = example_batch.coords['time'].values.astype(np.int64)
+print( f" Batch time coord({time.dtype}): {(time-time[0]).tolist()}")
 
 print("Loaded Batch:")
 for vname, dvar in example_batch.data_vars.items():
