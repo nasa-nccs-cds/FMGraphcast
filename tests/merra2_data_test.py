7952723a674176082518d7eccfc37aa8e6cdbd22
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-11 15:18:34 -0700

    Add small era5 forecast demo.

--- tests/merra2_data_test.py
+++ tests/merra2_data_test.py
@@ -92,9 +92,9 @@ lat, lon = stacked_inputs.coords['lat'].values, stacked_inputs.coords['lon'].val
 latf, lonf = np.cos( lat*(np.pi/90.0) ), np.sin( lon*(np.pi/90.0) )
 print( f"\n ** STACKED INPUTS {stacked_inputs.dims}: shape: {stacked_inputs.shape}, dtype: {stacked_inputs.dtype}, range: ({ndvar.min():.3f},{ndvar.max():.3f}), mean,std: ({ndvar.mean():.3f},{ndvar.std():.3f})")
 print( f"\n ---------- lat{lat.shape}:  {lat}"  )
-print( f"\n ---------- latf{latf.shape}: {latf}" )
+print( f"\n ---------- latf{latf.shape}: {[f'{v:.2f}' for v in latf]}" )
 print( f"\n ---------- lon{lon.shape}:  {lon}"  )
-print( f"\n ---------- lonf{lonf.shape}: {lonf}" )
+print( f"\n ---------- lonf{lonf.shape}: {[f'{v:.2f}' for v in lonf]}" )
 
 
 
