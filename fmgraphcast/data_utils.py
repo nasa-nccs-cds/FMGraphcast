09ed359eddab3fbc151f3a09552c058617577926
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-18 12:42:00 -0700

    Add small era5 forecast demo.

--- fmgraphcast/data_utils.py
+++ fmgraphcast/data_utils.py
@@ -249,7 +249,7 @@ def extract_inputs_targets_forcings(
 
   # `datetime` is needed by add_derived_vars but breaks autoregressive rollouts.
   dataset = dataset.drop_vars("datetime")
-  inputs, targets = extract_input_target_times( dataset, input_duration=input_duration, target_lead_times=target_lead_times)
+  inputs, targets = extract_input_target_times( dataset, input_duration=input_duration, target_lead_times=target_lead_times )
   print( f"Extract Inputs & Targets: input times: {get_timedeltas(inputs)}, target times: {get_timedeltas(targets)}")
 
   if set(forcing_variables) & set(target_variables):
