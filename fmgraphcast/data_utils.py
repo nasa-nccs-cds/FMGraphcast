2dcc7fde28de1f679c92a91cf3894be23d945809
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-18 12:26:36 -0700

    Add small era5 forecast demo.

--- fmgraphcast/data_utils.py
+++ fmgraphcast/data_utils.py
@@ -248,6 +248,7 @@ def extract_inputs_targets_forcings(
   dataset = dataset.drop_vars("datetime")
 
   inputs, targets = extract_input_target_times( dataset, input_duration=input_duration, target_lead_times=target_lead_times)
+  print( f"Extract Inputs & Targets: input times: {inputs.coords['time'].values.tolist()}, target times: {targets.coords['time'].values.tolist()}")
 
   if set(forcing_variables) & set(target_variables):
     raise ValueError( f"Forcing variables {forcing_variables} should not overlap with target variables {target_variables}." )
